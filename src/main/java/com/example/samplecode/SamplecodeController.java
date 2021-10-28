57fa59bdc960f4f8a2b204234240ca2960074523
suprabuddha <suprabuddha.nag@gmail.com>
2021-10-28 20:14:34 +0530

    Update on SimplecodeController and logging info

--- src/main/java/com/example/samplecode/SamplecodeController.java
+++ src/main/java/com/example/samplecode/SamplecodeController.java
@@ -1,14 +1,19 @@
 package com.example.samplecode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SamplecodeController {
 
+    Logger logger = LoggerFactory.getLogger(SamplecodeController.class);
     @GetMapping("/hello")
     public String sayHello(){
-        return "Welcome to Azure Web App in Production site";
+
+        logger.trace("Inside Controller");
+        return "Welcome to Azure Web App in the Production site";
     }
 
 }
